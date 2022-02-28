16965ca85d6c04c72c6b01acfa04ab21fa2f4c84
Taylor Huffman <taylor@crucialartists.com>
2022-02-28 10:48:31 -0600

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+
+    function focusHandler() {
+        console.log('Good!')
+    }
+
+    function blurHandler() {
+        console.log('Hey! Eyes on me!')
+    }
+
+    return (
+        <button onFocus={focusHandler} onBlur={blurHandler} >Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
