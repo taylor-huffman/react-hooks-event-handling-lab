e0c6d8f1415b4dcb0ccac1b49ed7a768689b9eff
Taylor Huffman <taylor@crucialartists.com>
2022-02-28 10:48:13 -0600

    commit tree 1c732eb6034ffd1e375955cf2b6fc7223304bd0c

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad() {
+    function handleChange() {
+        console.log('Entering password...')
+    }
+
+    return (
+        <input onChange={handleChange} type="password" />
+    )
+}
+
+export default Keypad
